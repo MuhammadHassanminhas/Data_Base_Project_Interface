8e5baf4cfbdfd4072331ed3d8dedc521d44d0dd1
Ali-Hassan-18 <ah282672@gmail.com>
2025-05-11 23:50:29 +0500

    stored procedure and making changes to database using buttons done

--- Design/Design/owner_employee_pay_check.Designer.cs
+++ Design/Design/owner_employee_pay_check.Designer.cs
@@ -30,239 +30,258 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(owner_employee_pay_check));
             panel1 = new Panel();
-            label4 = new Label();
-            textBox9 = new TextBox();
-            button1 = new Button();
-            ADD_BUTTON = new Button();
-            label8 = new Label();
-            textBox8 = new TextBox();
-            label9 = new Label();
-            textBox4 = new TextBox();
-            label5 = new Label();
-            textBox5 = new TextBox();
-            label6 = new Label();
-            textBox2 = new TextBox();
-            label3 = new Label();
-            textBox1 = new TextBox();
-            label1 = new Label();
-            service_name = new TextBox();
-            label2 = new Label();
+            netsalary = new Label();
+            deduction = new Label();
+            tips = new Label();
+            absents = new Label();
+            salaryperday = new Label();
+            name = new Label();
             dateTimePicker1 = new DateTimePicker();
+            label4 = new Label();
+            CALCULATE = new Button();
+            RELEASE_BUTTON = new Button();
+            label8 = new Label();
+            label9 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label3 = new Label();
+            id = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.White;
+            panel1.Controls.Add(netsalary);
+            panel1.Controls.Add(deduction);
+            panel1.Controls.Add(tips);
+            panel1.Controls.Add(absents);
+            panel1.Controls.Add(salaryperday);
+            panel1.Controls.Add(name);
             panel1.Controls.Add(dateTimePicker1);
             panel1.Controls.Add(label4);
-            panel1.Controls.Add(textBox9);
-            panel1.Controls.Add(button1);
-            panel1.Controls.Add(ADD_BUTTON);
+            panel1.Controls.Add(CALCULATE);
+            panel1.Controls.Add(RELEASE_BUTTON);
             panel1.Controls.Add(label8);
-            panel1.Controls.Add(textBox8);
             panel1.Controls.Add(label9);
-            panel1.Controls.Add(textBox4);
             panel1.Controls.Add(label5);
-            panel1.Controls.Add(textBox5);
             panel1.Controls.Add(label6);
-            panel1.Controls.Add(textBox2);
             panel1.Controls.Add(label3);
-            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(id);
             panel1.Controls.Add(label1);
-            panel1.Controls.Add(service_name);
             panel1.Controls.Add(label2);
-            panel1.Location = new Point(21, 24);
+            panel1.Location = new Point(24, 32);
+            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1055, 541);
+            panel1.Size = new Size(1206, 721);
             panel1.TabIndex = 0;
             panel1.Paint += panel1_Paint;
+            // 
+            // netsalary
+            // 
+            netsalary.AutoSize = true;
+            netsalary.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            netsalary.Location = new Point(942, 379);
+            netsalary.Name = "netsalary";
+            netsalary.Size = new Size(0, 31);
+            netsalary.TabIndex = 32;
+            // 
+            // deduction
+            // 
+            deduction.AutoSize = true;
+            deduction.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            deduction.Location = new Point(931, 225);
+            deduction.Name = "deduction";
+            deduction.Size = new Size(0, 31);
+            deduction.TabIndex = 31;
+            // 
+            // tips
+            // 
+            tips.AutoSize = true;
+            tips.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tips.Location = new Point(491, 224);
+            tips.Name = "tips";
+            tips.Size = new Size(0, 31);
+            tips.TabIndex = 30;
+            // 
+            // absents
+            // 
+            absents.AutoSize = true;
+            absents.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            absents.Location = new Point(155, 222);
+            absents.Name = "absents";
+            absents.Size = new Size(0, 31);
+            absents.TabIndex = 29;
+            // 
+            // salaryperday
+            // 
+            salaryperday.AutoSize = true;
+            salaryperday.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            salaryperday.Location = new Point(962, 39);
+            salaryperday.Name = "salaryperday";
+            salaryperday.Size = new Size(0, 31);
+            salaryperday.TabIndex = 28;
+            // 
+            // name
+            // 
+            name.AutoSize = true;
+            name.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            name.Location = new Point(515, 40);
+            name.Name = "name";
+            name.Size = new Size(0, 31);
+            name.TabIndex = 27;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(206, 377);
+            dateTimePicker1.Margin = new Padding(3, 4, 3, 4);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(259, 27);
+            dateTimePicker1.TabIndex = 26;
             // 
             // label4
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Arial", 14.25F, FontStyle.Bold);
             label4.ForeColor = Color.FromArgb(255, 136, 183);
-            label4.Location = new Point(671, 284);
+            label4.Location = new Point(767, 379);
             label4.Name = "label4";
-            label4.Size = new Size(130, 22);
+            label4.Size = new Size(164, 29);
             label4.TabIndex = 25;
             label4.Text = "NET SALARY";
             // 
-            // textBox9
+            // CALCULATE
             // 
-            textBox9.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
-            textBox9.Location = new Point(824, 283);
-            textBox9.Name = "textBox9";
-            textBox9.Size = new Size(206, 40);
-            textBox9.TabIndex = 24;
+            CALCULATE.AutoSize = true;
+            CALCULATE.BackColor = Color.FromArgb(255, 136, 183);
+            CALCULATE.FlatAppearance.BorderColor = Color.FromArgb(255, 136, 183);
+            CALCULATE.FlatAppearance.BorderSize = 0;
+            CALCULATE.FlatStyle = FlatStyle.Flat;
+            CALCULATE.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            CALCULATE.ForeColor = Color.White;
+            CALCULATE.Location = new Point(27, 525);
+            CALCULATE.Margin = new Padding(3, 4, 3, 4);
+            CALCULATE.Name = "CALCULATE";
+            CALCULATE.Size = new Size(229, 60);
+            CALCULATE.TabIndex = 23;
+            CALCULATE.Text = "CALCULATE";
+            CALCULATE.UseVisualStyleBackColor = false;
+            CALCULATE.Click += button1_Click;
             // 
-            // button1
+            // RELEASE_BUTTON
             // 
-            button1.AutoSize = true;
-            button1.BackColor = Color.FromArgb(255, 136, 183);
-            button1.FlatAppearance.BorderColor = Color.FromArgb(255, 136, 183);
-            button1.FlatAppearance.BorderSize = 0;
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button1.ForeColor = Color.White;
-            button1.Location = new Point(24, 394);
-            button1.Name = "button1";
-            button1.Size = new Size(157, 37);
-            button1.TabIndex = 23;
-            button1.Text = "CALCULATE";
-            button1.UseVisualStyleBackColor = false;
-            // 
-            // ADD_BUTTON
-            // 
-            ADD_BUTTON.AutoSize = true;
-            ADD_BUTTON.BackColor = Color.FromArgb(255, 136, 183);
-            ADD_BUTTON.FlatAppearance.BorderColor = Color.FromArgb(255, 136, 183);
-            ADD_BUTTON.FlatAppearance.BorderSize = 0;
-            ADD_BUTTON.FlatStyle = FlatStyle.Flat;
-            ADD_BUTTON.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            ADD_BUTTON.ForeColor = Color.White;
-            ADD_BUTTON.Location = new Point(213, 394);
-            ADD_BUTTON.Name = "ADD_BUTTON";
-            ADD_BUTTON.Size = new Size(131, 37);
-            ADD_BUTTON.TabIndex = 22;
-            ADD_BUTTON.Text = "RELEASE";
-            ADD_BUTTON.UseVisualStyleBackColor = false;
+            RELEASE_BUTTON.AutoSize = true;
+            RELEASE_BUTTON.BackColor = Color.FromArgb(255, 136, 183);
+            RELEASE_BUTTON.FlatAppearance.BorderColor = Color.FromArgb(255, 136, 183);
+            RELEASE_BUTTON.FlatAppearance.BorderSize = 0;
+            RELEASE_BUTTON.FlatStyle = FlatStyle.Flat;
+            RELEASE_BUTTON.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            RELEASE_BUTTON.ForeColor = Color.White;
+            RELEASE_BUTTON.Location = new Point(262, 525);
+            RELEASE_BUTTON.Margin = new Padding(3, 4, 3, 4);
+            RELEASE_BUTTON.Name = "RELEASE_BUTTON";
+            RELEASE_BUTTON.Size = new Size(186, 60);
+            RELEASE_BUTTON.TabIndex = 22;
+            RELEASE_BUTTON.Text = "RELEASE";
+            RELEASE_BUTTON.UseVisualStyleBackColor = false;
+            RELEASE_BUTTON.Click += ADD_BUTTON_Click;
             // 
             // label8
             // 
             label8.AutoSize = true;
             label8.Font = new Font("Arial", 14.25F, FontStyle.Bold);
             label8.ForeColor = Color.FromArgb(255, 136, 183);
-            label8.Location = new Point(24, 284);
+            label8.Location = new Point(27, 377);
             label8.Name = "label8";
-            label8.Size = new Size(138, 22);
+            label8.Size = new Size(173, 29);
             label8.TabIndex = 18;
             label8.Text = "MONTH/YEAR";
-            // 
-            // textBox8
-            // 
-            textBox8.Location = new Point(823, 174);
-            textBox8.Name = "textBox8";
-            textBox8.Size = new Size(206, 23);
-            textBox8.TabIndex = 17;
             // 
             // label9
             // 
             label9.AutoSize = true;
             label9.Font = new Font("Arial", 14.25F, FontStyle.Bold);
             label9.ForeColor = Color.FromArgb(255, 136, 183);
-            label9.Location = new Point(675, 180);
+            label9.Location = new Point(767, 225);
             label9.Name = "label9";
-            label9.Size = new Size(124, 22);
+            label9.Size = new Size(154, 29);
             label9.TabIndex = 16;
             label9.Text = "DEDUCTION";
-            // 
-            // textBox4
-            // 
-            textBox4.Location = new Point(445, 170);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(206, 23);
-            textBox4.TabIndex = 13;
             // 
             // label5
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Arial", 14.25F, FontStyle.Bold);
             label5.ForeColor = Color.FromArgb(255, 136, 183);
-            label5.Location = new Point(367, 171);
+            label5.Location = new Point(418, 225);
             label5.Name = "label5";
-            label5.Size = new Size(54, 22);
+            label5.Size = new Size(67, 29);
             label5.TabIndex = 12;
             label5.Text = "TIPS";
-            // 
-            // textBox5
-            // 
-            textBox5.Location = new Point(137, 168);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(206, 23);
-            textBox5.TabIndex = 11;
             // 
             // label6
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Arial", 14.25F, FontStyle.Bold);
             label6.ForeColor = Color.FromArgb(255, 136, 183);
-            label6.Location = new Point(24, 169);
+            label6.Location = new Point(27, 224);
             label6.Name = "label6";
-            label6.Size = new Size(89, 22);
+            label6.Size = new Size(111, 29);
             label6.TabIndex = 10;
             label6.Text = "ABSENT";
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(821, 33);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(206, 23);
-            textBox2.TabIndex = 9;
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Arial", 14.25F, FontStyle.Bold);
             label3.ForeColor = Color.FromArgb(255, 136, 183);
-            label3.Location = new Point(639, 34);
+            label3.Location = new Point(723, 41);
             label3.Name = "label3";
-            label3.Size = new Size(163, 22);
+            label3.Size = new Size(219, 29);
             label3.TabIndex = 8;
-            label3.Text = "SALARYPERDAY";
+            label3.Text = "SALARY PER DAY";
             // 
-            // textBox1
+            // id
             // 
-            textBox1.Location = new Point(414, 31);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(206, 23);
-            textBox1.TabIndex = 7;
+            id.Location = new Point(82, 37);
+            id.Margin = new Padding(3, 4, 3, 4);
+            id.Name = "id";
+            id.Size = new Size(235, 27);
+            id.TabIndex = 7;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Arial", 14.25F, FontStyle.Bold);
             label1.ForeColor = Color.FromArgb(255, 136, 183);
-            label1.Location = new Point(366, 32);
+            label1.Location = new Point(27, 39);
             label1.Name = "label1";
-            label1.Size = new Size(29, 22);
+            label1.Size = new Size(37, 29);
             label1.TabIndex = 6;
             label1.Text = "ID";
-            // 
-            // service_name
-            // 
-            service_name.Location = new Point(141, 29);
-            service_name.Name = "service_name";
-            service_name.Size = new Size(206, 23);
-            service_name.TabIndex = 5;
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Arial", 14.25F, FontStyle.Bold);
             label2.ForeColor = Color.FromArgb(255, 136, 183);
-            label2.Location = new Point(56, 31);
+            label2.Location = new Point(418, 41);
             label2.Name = "label2";
-            label2.Size = new Size(66, 22);
+            label2.Size = new Size(84, 29);
             label2.TabIndex = 4;
             label2.Text = "NAME";
             // 
-            // dateTimePicker1
-            // 
-            dateTimePicker1.Location = new Point(177, 284);
-            dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(200, 23);
-            dateTimePicker1.TabIndex = 26;
-            // 
             // owner_employee_pay_check
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
-            ClientSize = new Size(1096, 588);
+            ClientSize = new Size(1253, 784);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "owner_employee_pay_check";
             Text = "owner_employee_pay_check";
             panel1.ResumeLayout(false);
@@ -273,23 +292,23 @@
         #endregion
 
         private Panel panel1;
-        private TextBox textBox2;
         private Label label3;
-        private TextBox textBox1;
+        private TextBox id;
         private Label label1;
-        private TextBox service_name;
         private Label label2;
         private Label label8;
-        private TextBox textBox8;
         private Label label9;
-        private TextBox textBox4;
         private Label label5;
-        private TextBox textBox5;
         private Label label6;
-        private Button button1;
-        private Button ADD_BUTTON;
-        private TextBox textBox9;
+        private Button CALCULATE;
+        private Button RELEASE_BUTTON;
         private Label label4;
         private DateTimePicker dateTimePicker1;
+        private Label netsalary;
+        private Label deduction;
+        private Label tips;
+        private Label absents;
+        private Label salaryperday;
+        private Label name;
     }
 }
--- Design/Design/owner_employee_pay_check.cs
+++ Design/Design/owner_employee_pay_check.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Design
 {
     public partial class owner_employee_pay_check : Form
     {
+        string connString = "server=localhost;uid=root;pwd=root;database=beauty_salon;";
         public owner_employee_pay_check()
         {
             InitializeComponent();
@@ -20,6 +22,60 @@ namespace Design
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int staffId = Convert.ToInt32(id.Text);
+            DateTime payPeriod = dateTimePicker1.Value.Date;
+
+            using (MySqlConnection conn = new MySqlConnection(connString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("GetEmployeePaycheck", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@p_StaffID", staffId);
+                cmd.Parameters.AddWithValue("@p_PayPeriod", payPeriod);
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    name.Text = reader["Name"].ToString();
+                    salaryperday.Text = reader["Salary_Per_Day"].ToString();
+                    absents.Text = reader["Absents"].ToString();
+                    tips.Text = reader["Tips"].ToString();
+                    deduction.Text = reader["Deduction"].ToString();
+                    netsalary.Text = reader["Net_Salary"].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("No paycheck record found for this employee and period.");
+                }
+
+                reader.Close();
+            }
+        }
+
+        private void ADD_BUTTON_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("InsertEmployeePaycheck", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@p_StaffID", Convert.ToInt32(id.Text));
+                cmd.Parameters.AddWithValue("@p_PayPeriod", dateTimePicker1.Value.Date);
+                cmd.Parameters.AddWithValue("@p_Salary_Per_Day", Convert.ToDecimal(salaryperday.Text));
+                cmd.Parameters.AddWithValue("@p_Absents", Convert.ToInt32(absents.Text));
+                cmd.Parameters.AddWithValue("@p_Tips", Convert.ToDecimal(tips.Text));
+                cmd.Parameters.AddWithValue("@p_Deduction", Convert.ToDecimal(deduction.Text));
+                cmd.Parameters.AddWithValue("@p_Net_Salary", Convert.ToDecimal(netsalary.Text));
+
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Paycheck released successfully.");
+            }
         }
     }
 }
