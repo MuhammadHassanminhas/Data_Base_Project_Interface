92065dac7c99498a666b85d8485c3499ea30b0fe
Ali-Hassan-18 <ah282672@gmail.com>
2025-05-11 15:12:22 +0500

    CRUD operations added and data displayed

--- Design/Design/services.Designer.cs
+++ Design/Design/services.Designer.cs
@@ -30,6 +30,10 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(services));
             panel1 = new Panel();
+            service_id = new TextBox();
+            label3 = new Label();
+            service_duration = new TextBox();
+            label1 = new Label();
             STOCK_dataGridView = new DataGridView();
             DELETE_BUTTON = new Button();
             EDIT_BUTTON = new Button();
@@ -45,6 +49,10 @@
             // panel1
             // 
             panel1.BackColor = Color.White;
+            panel1.Controls.Add(service_id);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(service_duration);
+            panel1.Controls.Add(label1);
             panel1.Controls.Add(STOCK_dataGridView);
             panel1.Controls.Add(DELETE_BUTTON);
             panel1.Controls.Add(EDIT_BUTTON);
@@ -53,18 +61,60 @@
             panel1.Controls.Add(label4);
             panel1.Controls.Add(service_name);
             panel1.Controls.Add(label2);
-            panel1.Location = new Point(21, 24);
+            panel1.Location = new Point(24, 32);
+            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1055, 541);
+            panel1.Size = new Size(1206, 721);
             panel1.TabIndex = 1;
+            panel1.Paint += panel1_Paint;
+            // 
+            // service_id
+            // 
+            service_id.Location = new Point(158, 149);
+            service_id.Margin = new Padding(3, 4, 3, 4);
+            service_id.Name = "service_id";
+            service_id.Size = new Size(235, 27);
+            service_id.TabIndex = 18;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 14.25F, FontStyle.Bold);
+            label3.ForeColor = Color.FromArgb(255, 136, 183);
+            label3.Location = new Point(18, 149);
+            label3.Name = "label3";
+            label3.Size = new Size(37, 29);
+            label3.TabIndex = 17;
+            label3.Text = "ID";
+            // 
+            // service_duration
+            // 
+            service_duration.Location = new Point(158, 309);
+            service_duration.Margin = new Padding(3, 4, 3, 4);
+            service_duration.Name = "service_duration";
+            service_duration.Size = new Size(235, 27);
+            service_duration.TabIndex = 16;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 14.25F, FontStyle.Bold);
+            label1.ForeColor = Color.FromArgb(255, 136, 183);
+            label1.Location = new Point(16, 309);
+            label1.Name = "label1";
+            label1.Size = new Size(136, 29);
+            label1.TabIndex = 15;
+            label1.Text = "DURATION";
             // 
             // STOCK_dataGridView
             // 
             STOCK_dataGridView.BackgroundColor = Color.FromArgb(253, 210, 227);
             STOCK_dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            STOCK_dataGridView.Location = new Point(408, 26);
+            STOCK_dataGridView.Location = new Point(466, 35);
+            STOCK_dataGridView.Margin = new Padding(3, 4, 3, 4);
             STOCK_dataGridView.Name = "STOCK_dataGridView";
-            STOCK_dataGridView.Size = new Size(627, 494);
+            STOCK_dataGridView.RowHeadersWidth = 51;
+            STOCK_dataGridView.Size = new Size(717, 659);
             STOCK_dataGridView.TabIndex = 14;
             // 
             // DELETE_BUTTON
@@ -76,12 +126,14 @@
             DELETE_BUTTON.FlatStyle = FlatStyle.Flat;
             DELETE_BUTTON.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
             DELETE_BUTTON.ForeColor = Color.White;
-            DELETE_BUTTON.Location = new Point(230, 290);
+            DELETE_BUTTON.Location = new Point(263, 387);
+            DELETE_BUTTON.Margin = new Padding(3, 4, 3, 4);
             DELETE_BUTTON.Name = "DELETE_BUTTON";
-            DELETE_BUTTON.Size = new Size(114, 37);
+            DELETE_BUTTON.Size = new Size(163, 60);
             DELETE_BUTTON.TabIndex = 12;
             DELETE_BUTTON.Text = "DELETE";
             DELETE_BUTTON.UseVisualStyleBackColor = false;
+            DELETE_BUTTON.Click += DELETE_BUTTON_Click;
             // 
             // EDIT_BUTTON
             // 
@@ -92,12 +144,14 @@
             EDIT_BUTTON.FlatStyle = FlatStyle.Flat;
             EDIT_BUTTON.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
             EDIT_BUTTON.ForeColor = Color.White;
-            EDIT_BUTTON.Location = new Point(122, 290);
+            EDIT_BUTTON.Location = new Point(139, 387);
+            EDIT_BUTTON.Margin = new Padding(3, 4, 3, 4);
             EDIT_BUTTON.Name = "EDIT_BUTTON";
-            EDIT_BUTTON.Size = new Size(102, 37);
+            EDIT_BUTTON.Size = new Size(117, 60);
             EDIT_BUTTON.TabIndex = 11;
             EDIT_BUTTON.Text = "EDIT";
             EDIT_BUTTON.UseVisualStyleBackColor = false;
+            EDIT_BUTTON.Click += EDIT_BUTTON_Click;
             // 
             // ADD_BUTTON
             // 
@@ -108,18 +162,21 @@
             ADD_BUTTON.FlatStyle = FlatStyle.Flat;
             ADD_BUTTON.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
             ADD_BUTTON.ForeColor = Color.White;
-            ADD_BUTTON.Location = new Point(14, 290);
+            ADD_BUTTON.Location = new Point(16, 387);
+            ADD_BUTTON.Margin = new Padding(3, 4, 3, 4);
             ADD_BUTTON.Name = "ADD_BUTTON";
-            ADD_BUTTON.Size = new Size(102, 37);
+            ADD_BUTTON.Size = new Size(117, 60);
             ADD_BUTTON.TabIndex = 10;
             ADD_BUTTON.Text = "ADD";
             ADD_BUTTON.UseVisualStyleBackColor = false;
+            ADD_BUTTON.Click += ADD_BUTTON_Click;
             // 
             // service_price
             // 
-            service_price.Location = new Point(139, 188);
+            service_price.Location = new Point(159, 251);
+            service_price.Margin = new Padding(3, 4, 3, 4);
             service_price.Name = "service_price";
-            service_price.Size = new Size(206, 23);
+            service_price.Size = new Size(235, 27);
             service_price.TabIndex = 8;
             // 
             // label4
@@ -127,17 +184,18 @@
             label4.AutoSize = true;
             label4.Font = new Font("Arial", 14.25F, FontStyle.Bold);
             label4.ForeColor = Color.FromArgb(255, 136, 183);
-            label4.Location = new Point(15, 186);
+            label4.Location = new Point(16, 251);
             label4.Name = "label4";
-            label4.Size = new Size(68, 22);
+            label4.Size = new Size(86, 29);
             label4.TabIndex = 7;
             label4.Text = "PRICE";
             // 
             // service_name
             // 
-            service_name.Location = new Point(138, 143);
+            service_name.Location = new Point(158, 191);
+            service_name.Margin = new Padding(3, 4, 3, 4);
             service_name.Name = "service_name";
-            service_name.Size = new Size(206, 23);
+            service_name.Size = new Size(235, 27);
             service_name.TabIndex = 3;
             // 
             // label2
@@ -145,22 +203,24 @@
             label2.AutoSize = true;
             label2.Font = new Font("Arial", 14.25F, FontStyle.Bold);
             label2.ForeColor = Color.FromArgb(255, 136, 183);
-            label2.Location = new Point(15, 143);
+            label2.Location = new Point(16, 191);
             label2.Name = "label2";
-            label2.Size = new Size(66, 22);
+            label2.Size = new Size(84, 29);
             label2.TabIndex = 2;
             label2.Text = "NAME";
             // 
             // services
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
-            ClientSize = new Size(1096, 588);
+            ClientSize = new Size(1253, 784);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "services";
             Text = "services";
+            Load += services_Load;
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)STOCK_dataGridView).EndInit();
@@ -178,5 +238,9 @@
         private Label label4;
         private TextBox service_name;
         private Label label2;
+        private TextBox service_duration;
+        private Label label1;
+        private TextBox service_id;
+        private Label label3;
     }
 }
--- Design/Design/services.cs
+++ Design/Design/services.cs
@@ -7,14 +7,90 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Xml.Linq;
 
 namespace Design
 {
-    public partial class services: Form
+    public partial class services : Form
     {
+        string connString = "server=localhost;uid=root;pwd=root;database=beauty_salon;";
+
         public services()
         {
             InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void ADD_BUTTON_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("InsertService", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@p_Name", service_name.Text);
+                cmd.Parameters.AddWithValue("@p_Price", Convert.ToDecimal(service_price.Text));
+                cmd.Parameters.AddWithValue("@p_Duration", Convert.ToInt16(service_duration.Text));
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Service Added!");
+                LoadServices();  // Refresh grid
+            }
+
+        }
+
+        private void EDIT_BUTTON_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("UpdateService", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@p_ServiceID", Convert.ToInt32(service_id.Text));
+                cmd.Parameters.AddWithValue("@p_Name", service_name.Text);
+                cmd.Parameters.AddWithValue("@p_Price", Convert.ToDecimal(service_price.Text));
+                cmd.Parameters.AddWithValue("@p_Duration", Convert.ToInt16(service_duration.Text));
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Service Updated!");
+                LoadServices();
+            }
+
+        }
+
+        private void DELETE_BUTTON_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("DeleteService", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@p_ServiceID", Convert.ToInt32(service_id.Text));
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Service Deleted!");
+                LoadServices();
+            }
+
+        }
+        private void LoadServices()
+        {
+            using (MySqlConnection conn = new MySqlConnection(connString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("GetAllServices", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                STOCK_dataGridView.DataSource = dt;
+            }
+        }
+
+        private void services_Load(object sender, EventArgs e)
+        {
+            LoadServices();
         }
     }
 }
