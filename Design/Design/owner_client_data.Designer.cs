d8adf10235998c903b1f9ae814db0ac4b6676919
Ali-Hassan-18 <ah282672@gmail.com>
2025-05-12 08:45:48 +0500

    added stored procedures and linked db and displayed the data properly

--- Design/Design/owner_client_data.Designer.cs
+++ Design/Design/owner_client_data.Designer.cs
@@ -30,46 +30,92 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(owner_client_data));
             panel1 = new Panel();
-            attendence_dataGridView = new DataGridView();
+            client_ID = new TextBox();
+            label3 = new Label();
+            client_address = new TextBox();
+            label1 = new Label();
+            CLIENT_dataGridView = new DataGridView();
             DELETE_BUTTON = new Button();
             EDIT_BUTTON = new Button();
             ADD_BUTTON = new Button();
-            service_price = new TextBox();
+            client_contact = new TextBox();
             label4 = new Label();
-            service_name = new TextBox();
+            client_name = new TextBox();
             label2 = new Label();
-            textBox1 = new TextBox();
-            label1 = new Label();
             panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)attendence_dataGridView).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)CLIENT_dataGridView).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.White;
-            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(client_ID);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(client_address);
             panel1.Controls.Add(label1);
-            panel1.Controls.Add(attendence_dataGridView);
+            panel1.Controls.Add(CLIENT_dataGridView);
             panel1.Controls.Add(DELETE_BUTTON);
             panel1.Controls.Add(EDIT_BUTTON);
             panel1.Controls.Add(ADD_BUTTON);
-            panel1.Controls.Add(service_price);
+            panel1.Controls.Add(client_contact);
             panel1.Controls.Add(label4);
-            panel1.Controls.Add(service_name);
+            panel1.Controls.Add(client_name);
             panel1.Controls.Add(label2);
-            panel1.Location = new Point(21, 24);
+            panel1.Location = new Point(24, 32);
+            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1055, 541);
+            panel1.Size = new Size(1206, 721);
             panel1.TabIndex = 4;
             // 
-            // attendence_dataGridView
+            // client_ID
             // 
-            attendence_dataGridView.BackgroundColor = Color.FromArgb(253, 210, 227);
-            attendence_dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            attendence_dataGridView.Location = new Point(20, 325);
-            attendence_dataGridView.Name = "attendence_dataGridView";
-            attendence_dataGridView.Size = new Size(1015, 213);
-            attendence_dataGridView.TabIndex = 14;
+            client_ID.Location = new Point(464, 18);
+            client_ID.Margin = new Padding(3, 4, 3, 4);
+            client_ID.Name = "client_ID";
+            client_ID.Size = new Size(235, 27);
+            client_ID.TabIndex = 18;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 14.25F, FontStyle.Bold);
+            label3.ForeColor = Color.FromArgb(255, 136, 183);
+            label3.Location = new Point(391, 16);
+            label3.Name = "label3";
+            label3.Size = new Size(37, 29);
+            label3.TabIndex = 17;
+            label3.Text = "ID";
+            // 
+            // client_address
+            // 
+            client_address.Location = new Point(240, 173);
+            client_address.Margin = new Padding(3, 4, 3, 4);
+            client_address.Multiline = true;
+            client_address.Name = "client_address";
+            client_address.Size = new Size(838, 29);
+            client_address.TabIndex = 16;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 14.25F, FontStyle.Bold);
+            label1.ForeColor = Color.FromArgb(255, 136, 183);
+            label1.Location = new Point(81, 173);
+            label1.Name = "label1";
+            label1.Size = new Size(129, 29);
+            label1.TabIndex = 15;
+            label1.Text = "ADDRESS";
+            // 
+            // CLIENT_dataGridView
+            // 
+            CLIENT_dataGridView.BackgroundColor = Color.FromArgb(253, 210, 227);
+            CLIENT_dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            CLIENT_dataGridView.Location = new Point(23, 433);
+            CLIENT_dataGridView.Margin = new Padding(3, 4, 3, 4);
+            CLIENT_dataGridView.Name = "CLIENT_dataGridView";
+            CLIENT_dataGridView.RowHeadersWidth = 51;
+            CLIENT_dataGridView.Size = new Size(1160, 284);
+            CLIENT_dataGridView.TabIndex = 14;
             // 
             // DELETE_BUTTON
             // 
@@ -80,12 +126,14 @@
             DELETE_BUTTON.FlatStyle = FlatStyle.Flat;
             DELETE_BUTTON.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
             DELETE_BUTTON.ForeColor = Color.White;
-            DELETE_BUTTON.Location = new Point(537, 252);
+            DELETE_BUTTON.Location = new Point(614, 336);
+            DELETE_BUTTON.Margin = new Padding(3, 4, 3, 4);
             DELETE_BUTTON.Name = "DELETE_BUTTON";
-            DELETE_BUTTON.Size = new Size(114, 37);
+            DELETE_BUTTON.Size = new Size(163, 60);
             DELETE_BUTTON.TabIndex = 12;
             DELETE_BUTTON.Text = "DELETE";
             DELETE_BUTTON.UseVisualStyleBackColor = false;
+            DELETE_BUTTON.Click += DELETE_BUTTON_Click;
             // 
             // EDIT_BUTTON
             // 
@@ -96,12 +144,14 @@
             EDIT_BUTTON.FlatStyle = FlatStyle.Flat;
             EDIT_BUTTON.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
             EDIT_BUTTON.ForeColor = Color.White;
-            EDIT_BUTTON.Location = new Point(429, 252);
+            EDIT_BUTTON.Location = new Point(490, 336);
+            EDIT_BUTTON.Margin = new Padding(3, 4, 3, 4);
             EDIT_BUTTON.Name = "EDIT_BUTTON";
-            EDIT_BUTTON.Size = new Size(102, 37);
+            EDIT_BUTTON.Size = new Size(117, 60);
             EDIT_BUTTON.TabIndex = 11;
             EDIT_BUTTON.Text = "SAVE";
             EDIT_BUTTON.UseVisualStyleBackColor = false;
+            EDIT_BUTTON.Click += EDIT_BUTTON_Click;
             // 
             // ADD_BUTTON
             // 
@@ -112,96 +162,84 @@
             ADD_BUTTON.FlatStyle = FlatStyle.Flat;
             ADD_BUTTON.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
             ADD_BUTTON.ForeColor = Color.White;
-            ADD_BUTTON.Location = new Point(321, 252);
+            ADD_BUTTON.Location = new Point(367, 336);
+            ADD_BUTTON.Margin = new Padding(3, 4, 3, 4);
             ADD_BUTTON.Name = "ADD_BUTTON";
-            ADD_BUTTON.Size = new Size(102, 37);
+            ADD_BUTTON.Size = new Size(117, 60);
             ADD_BUTTON.TabIndex = 10;
             ADD_BUTTON.Text = "ADD";
             ADD_BUTTON.UseVisualStyleBackColor = false;
+            ADD_BUTTON.Click += ADD_BUTTON_Click;
             // 
-            // service_price
+            // client_contact
             // 
-            service_price.Location = new Point(765, 45);
-            service_price.Name = "service_price";
-            service_price.Size = new Size(206, 23);
-            service_price.TabIndex = 8;
+            client_contact.Location = new Point(843, 90);
+            client_contact.Margin = new Padding(3, 4, 3, 4);
+            client_contact.Name = "client_contact";
+            client_contact.Size = new Size(235, 27);
+            client_contact.TabIndex = 8;
             // 
             // label4
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Arial", 14.25F, FontStyle.Bold);
             label4.ForeColor = Color.FromArgb(255, 136, 183);
-            label4.Location = new Point(500, 45);
+            label4.Location = new Point(558, 90);
             label4.Name = "label4";
-            label4.Size = new Size(192, 22);
+            label4.Size = new Size(239, 29);
             label4.TabIndex = 7;
             label4.Text = "CONTACT NUMBER";
             // 
-            // service_name
+            // client_name
             // 
-            service_name.Location = new Point(221, 42);
-            service_name.Name = "service_name";
-            service_name.Size = new Size(206, 23);
-            service_name.TabIndex = 3;
+            client_name.Location = new Point(240, 90);
+            client_name.Margin = new Padding(3, 4, 3, 4);
+            client_name.Name = "client_name";
+            client_name.Size = new Size(235, 27);
+            client_name.TabIndex = 3;
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Arial", 14.25F, FontStyle.Bold);
             label2.ForeColor = Color.FromArgb(255, 136, 183);
-            label2.Location = new Point(82, 42);
+            label2.Location = new Point(81, 90);
             label2.Name = "label2";
-            label2.Size = new Size(66, 22);
+            label2.Size = new Size(84, 29);
             label2.TabIndex = 2;
             label2.Text = "NAME";
             // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(221, 106);
-            textBox1.Multiline = true;
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(430, 23);
-            textBox1.TabIndex = 16;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Arial", 14.25F, FontStyle.Bold);
-            label1.ForeColor = Color.FromArgb(255, 136, 183);
-            label1.Location = new Point(82, 107);
-            label1.Name = "label1";
-            label1.Size = new Size(103, 22);
-            label1.TabIndex = 15;
-            label1.Text = "ADDRESS";
-            // 
             // owner_client_data
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
-            ClientSize = new Size(1096, 588);
+            ClientSize = new Size(1253, 784);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "owner_client_data";
             Text = "owner_client_data";
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)attendence_dataGridView).EndInit();
+            ((System.ComponentModel.ISupportInitialize)CLIENT_dataGridView).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel panel1;
-        private DataGridView attendence_dataGridView;
+        private DataGridView CLIENT_dataGridView;
         private Button DELETE_BUTTON;
         private Button EDIT_BUTTON;
         private Button ADD_BUTTON;
-        private TextBox service_price;
+        private TextBox client_contact;
         private Label label4;
-        private TextBox service_name;
+        private TextBox client_name;
         private Label label2;
-        private TextBox textBox1;
+        private TextBox client_address;
         private Label label1;
+        private TextBox client_ID;
+        private Label label3;
     }
 }
